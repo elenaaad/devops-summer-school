92ea7238705ab91693fcd7eeff2b8f0f089fa5df
Tania <tania.g@adoreme.com>
2024-06-30 21:10:12 +0300

    Last commit

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
+# Start here
+
+## Terraform Setup Instructions
+
+### Modify `main.tf` Accordingly
+
+1. **Create a Cloud Run v2 Service**
+    - Name: `cr_service_INITIALE`
+    - Use image: `us-docker.pkg.dev/cloudrun/container/hello`
+    - Set CPU to `2`
+    - Set memory to `1024Mi`
+
+2. **Create a Service Account**
+    - Name: `cloud_run_invoker_INITIALE`
+    - Use the project name from variables
+
+3. **Bind the Previously Created Service Account**
+    - Use the Service Account ID from computed attributes that are exported from the previous step
+    - Add the Invoker role to the Service Account. You can search for the Invoker role [here](https://cloud.google.com/run/docs/reference/iam/roles)
+    - Use the Service Account email from computed attributes that are exported from the previous step
+
+4. **Make your Cloud Run service internet accesible**
+    - Switch from GCP interface your Cloud Run setting - Authentication - Allow unauthenticated invocations 
+
+
 # Hints for later
 
 🔧 Access terraform resources [here](https://registry.terraform.io/providers/hashicorp/google/latest/docs).
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-# DELETE THIS LINE AND INSERT YOUR CODE HERE 👇🏻
+# INSERT YOUR CODE HERE 👇🏻
+# create a Cloud Run resource
 
-resource "google_cloud_run_v2_service" "first-service" {
+resource "google_cloud_run_v2_service" "first_service" {
   name     = "cloudrun-service"
   location = "us-central1"
   ingress  = "INGRESS_TRAFFIC_ALL"
@@ -10,10 +11,26 @@ resource "google_cloud_run_v2_service" "first-service" {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
       resources {
         limits = {
-          cpu    = "2"
+          cpu    = "1"
           memory = "1024Mi"
         }
       }
     }
   }
+}
+
+# create a Service Account resource
+
+resource "google_service_account" "cloud_run_invoker_sa" {
+  account_id   = "cloud-run-invoker"
+  display_name = "Cloud Run Invoker"
+  project      = var.project_id
+}
+
+#bind it to invoker role
+
+resource "google_service_account_iam_binding" "run_invoker_binding" {
+  service_account_id = google_service_account.cloud_run_invoker_sa.name
+  role               = "roles/run.invoker"
+  members            = ["serviceAccount:${google_service_account.cloud_run_invoker_sa.email}"]
 }
