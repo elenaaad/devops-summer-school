06bf2005cfb36c9460a74064059aa6dc79ad9ad5
elenaaad <94315701+elenaaad@users.noreply.github.com>
2024-07-01 12:36:04 +0300

    first commit

--- main.tf
+++ main.tf
@@ -1,6 +1,24 @@
 # INSERT YOUR CODE HERE 👇🏻
 # create a Cloud Run resource
 
+resource "google_cloud_run_v2_service" "summer_school" {
+  name     = "cloudrun-service"
+  location = "us-central1"
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      resources {
+        limits = {
+          cpu    = "2"
+          memory = "1024Mi"
+        }
+      }
+    }
+  }
+}
+
 
 # create a Service Account resource
 
